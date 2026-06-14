f646cf3ec53e38346f80ecbe1dd8ae5a6092369d
Srikanth <80593148+srikanthgudala8@users.noreply.github.com>
2026-06-14 23:19:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (verify latest AMI)
   instance_type = "t2.micro"
