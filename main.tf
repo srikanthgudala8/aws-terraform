189d9edd75344baddcf7a559fadba03b5e4bdfef
Srikanth <80593148+srikanthgudala8@users.noreply.github.com>
2026-06-14 23:18:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (verify latest AMI)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
 }
