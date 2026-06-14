0ee8bd4401f30e49ef191a2f2096add6e74a63a3
Srikanth <80593148+srikanthgudala8@users.noreply.github.com>
2026-06-14 23:18:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (verify latest AMI)
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-EC2"
-  }
-}
+r
